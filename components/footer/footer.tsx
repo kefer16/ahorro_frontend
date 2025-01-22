b1b570122469ba706b3a6a194fb24ce00db728d5
Kevin Morales <66371768+kefer16@users.noreply.github.com>
2025-01-21 23:55:21 -0500

    fixed theme and footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -4,7 +4,7 @@ import { Separator } from "../ui/separator"
 
 function Footer() {
    return (
-      <footer className="absolute bottom-0 left-0 w-full h-[60px] flex justify-around shadow-sm bg-card">
+      <footer className="fixed bottom-0 left-0 w-full h-[60px] flex justify-around shadow-sm bg-card">
          <div className="w-full">
             <Separator />
             <div className="w-full flex gap-2 px-4 pb-3 pt-2">
