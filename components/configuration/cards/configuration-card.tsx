9748088d873001e1e67e87056afb5a21dc24b96e
Kevin Morales <66371768+kefer16@users.noreply.github.com>
2025-01-22 00:18:01 -0500

    fixed configuration

--- components/configuration/cards/configuration-card.tsx
+++ components/configuration/cards/configuration-card.tsx
@@ -8,25 +8,34 @@ function ConfigurationCard({ isDanger, icon, link, title, description }: Configu
          {
             isDanger ?
                <Link href={link} className="flex justify-between items-center rounded-xl border bg-red-500/10 border-red-500/30 p-4">
-                  <div className="flex gap-4 items-center">
-                     {icon}
-                     <div className="flex flex-col">
+                  <div className="flex gap-4 items-center w-full">
+                     <div className="flex justify-center items-center w-[30px]">
+                        {icon}
+                     </div>
+                     <div className="flex flex-col w-full">
                         <p className="font-semibold text-md text-red-500">{title}</p>
                         <p className="text-muted-foreground font-normal text-sm">{description}</p>
                      </div>
                   </div>
-                  <ChevronRight size={22} className="text-red-500" />
+                  <div className="flex justify-center items-center w-[30px]">
+                     <ChevronRight size={22} className="text-red-500" />
+                  </div>
                </Link>
                :
-               <Link href={link} className="flex justify-between items-center rounded-xl border bg-card p-4">
-                  <div className="flex gap-4 items-center">
-                     {icon}
-                     <div className="flex flex-col">
+               <Link href={link} className="flex justify-between gap-4 items-center rounded-xl border bg-card p-4">
+                  <div className="flex gap-4 items-center w-full">
+                     <div className="flex justify-center items-center w-[30px]">
+                        {icon}
+                     </div>
+                     <div className="flex flex-col w-full">
                         <p className="font-semibold text-md">{title}</p>
                         <p className="text-muted-foreground font-normal text-sm">{description}</p>
                      </div>
                   </div>
-                  <ChevronRight size={22} className="text-muted-foreground" />
+                  <div className="flex justify-center items-center w-[30px]">
+                     <ChevronRight size={22} className="text-muted-foreground" />
+                  </div>
+
                </Link>
          }
       </>
